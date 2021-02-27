1ae3db23e2b729cdff427cf3405723b30ef5cb30
Your Name <you@example.com>
2021-02-26 18:48:33 -0600

    update docker run

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -1,17 +1,15 @@
-alias drun='sudo docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v /data:/data -v /nfs_megatron:/nfs_megatron'
-alias drun_nodevice='sudo docker run -it --rm --network=host --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v /data:/data -v /nfs_megatron:/nfs_megatron'
+alias drun='docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
+alias drun_nodevice='docker run -it --rm --network=host --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx'
 
 # WORK_DIR=/var/lib/jenkins/pytorch
 # WORK_DIR='/dockerx/pytorch'
 WORK_DIR='/root/pytorch'
 
-# drun -w $WORK_DIR rocm/pytorch
-# drun -w $WORK_DIR rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
-# drun -w $WORK_DIR rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch_1.7.0
-# drun -w $WORK_DIR rocm/pytorch-private:rocm-3221-pytorch-rocblas-tuned
-# drun -w $WORK_DIR rocm/pytorch-private:rocm-3221-pytorch-rocblas-tuned-rnnfp16-miopen
-# drun -w $WORK_DIR rnn_test
+# IMAGE_NAME=rocm/pytorch
+# IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
+# IMAGE_NAME=rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
+IMAGE_NAME=rocm/pytorch-private:rocm-3221-pytorch-rocblas-tuning-lstm-fp16-rnnfp16-miopen
 
-drun -d --name rnnfp16_container -w $WORK_DIR rocm/pytorch-private:rocm4.0.1_ubuntu18.04_py3.6_pytorch-rocblas-tuned-rnnfp16-miopen
-# docker cp . rnnfp16_container:/root/pytorch
-docker attach rnnfp16_container
+drun -d --name pytorch_container -w $WORK_DIR $IMAGE_NAME
+# docker cp . pytorch_container:/root/pytorch
+docker attach pytorch_container
