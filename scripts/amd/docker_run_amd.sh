a0759bde85132119bccccb7988fed5cc187f6d9d
Michael Melesse <micmelesse@gmail.com>
2021-04-21 16:58:28 -0700

    add 4.1.1 image

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -3,18 +3,19 @@ alias drun_nodevice='sudo docker run -it --rm --network=host --ipc=host --shm-si
 
 VOLUMES="-v $HOME/dockerx:/dockerx -v /data:/data"
 
-# WORK_DIR='-w /var/lib/jenkins/pytorch'
-# WORK_DIR='-w /dockerx/pytorch'
-WORK_DIR='-w /root/pytorch'
+# WORK_DIR='/var/lib/jenkins/pytorch'
+# WORK_DIR='/dockerx/pytorch'
+WORK_DIR='/root/pytorch'
 
 # IMAGE_NAME=rocm/pytorch
 # IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
 # IMAGE_NAME=rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
 IMAGE_NAME=rocm/pytorch-private:rocm4.0.1_ubuntu18.04_py3.6_pytorch_hipfft_issue_4
+# IMAGE_NAME=rocm/pytorch:rocm4.1.1_ubuntu18.04_py3.6_pytorch
 
-CONTAINER_ID=$(drun -d $WORK_DIR $VOLUMES $IMAGE_NAME)
+CONTAINER_ID=$(drun -d -w $WORK_DIR $VOLUMES $IMAGE_NAME)
 echo "CONTAINER_ID: $CONTAINER_ID"
-docker cp . $CONTAINER_ID:/root/pytorch
+docker cp . $CONTAINER_ID:$WORK_DIR
 docker attach $CONTAINER_ID
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
