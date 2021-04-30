95ad2adba55e2e33a9c45c52c164a95082138a9b
Michael Melesse <micmelesse@gmail.com>
2021-04-30 21:31:39 +0000

    update test script

--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -1,1 +1,11 @@
 echo "testing"
+
+# PYTORCH_TEST_WITH_ROCM=1 python test/test_spectral_ops.py --verbose \
+#     2>&1 | tee scripts/amd/test_spectral.log
+
+# python test/test_spectral_ops.py --verbose \
+#     2>&1 | tee scripts/amd/test_spectral.log
+
+## FAILING TEST
+
+python test/test_spectral_ops.py --verbose TestFFTCUDA.test_cufft_plan_cache_cuda_float64
