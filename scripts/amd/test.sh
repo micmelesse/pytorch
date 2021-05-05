d414c3d65b88f84433c3b2131fbf89f7d60000e9
Michael Melesse <micmelesse@gmail.com>
2021-05-05 22:43:11 +0000

    add tests

--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -1,1 +1,16 @@
 echo "testing"
+
+# PYTORCH_TEST_WITH_ROCM=1 python test/test_spectral_ops.py --verbose \
+#     2>&1 | tee scripts/amd/test_spectral.log
+
+# python test/test_spectral_ops.py --verbose \
+#     2>&1 | tee scripts/amd/test_spectral.log
+
+
+# python test/test_spectral_ops.py --verbose TestFFTCUDA.test_reference_nd_fft_irfftn_cuda_complex128 #hipfftExecZ2D
+
+# python test/test_spectral_ops.py --verbose TestFFTCUDA.test_reference_nd_fft_irfftn_cuda_complex64 #hipfftExecC2R
+
+# python test/test_spectral_ops.py --verbose TestFFTCUDA.test_reference_nd_fft_irfftn_cuda_float64 #hipfftExecZ2D
+
+python test/test_spectral_ops.py --verbose TestFFTCUDA.test_reference_nd_fft_irfftn_cuda_float32 #hipfftExecC2R
