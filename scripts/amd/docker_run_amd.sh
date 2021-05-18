aa51076dcd9e1bafc6b08bb329950cbe2680dc8a
Michael Melesse <micmelesse@gmail.com>
2021-05-18 20:01:18 +0000

    update script

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -1,7 +1,10 @@
 set -o xtrace
 
-alias drun='sudo docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
-alias drun_nodevice='sudo docker run -it --rm --network=host --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
+alias drun='sudo docker run -it --rm --network=host --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
+
+DEVICES="--device=/dev/kfd --device=/dev/dri"
+
+MEMORY="--ipc=host --shm-size 16G"
 
 VOLUMES="-v $HOME/dockerx:/dockerx -v /data:/data"
 
@@ -16,7 +19,7 @@ WORK_DIR='/root/pytorch'
 IMAGE_NAME=compute-artifactory.amd.com:5000/rocm-plus-docker/framework/compute-rocm-rel-4.2:16_ubuntu18.04_py3.6_pytorch_rocm4.2_internal_testing_b2c58d0_18
 
 
-CONTAINER_ID=$(drun -d -w $WORK_DIR $VOLUMES $IMAGE_NAME)
+CONTAINER_ID=$(drun -d -w $WORK_DIR $MEMORY $VOLUMES $DEVICES $IMAGE_NAME)
 echo "CONTAINER_ID: $CONTAINER_ID"
 docker cp . $CONTAINER_ID:$WORK_DIR
 docker attach $CONTAINER_ID
--- scripts/amd/git_remote_add.sh
+++ scripts/amd/git_remote_add.sh
@@ -1,3 +1,6 @@
-git remote add upstream https://github.com/pytorch/pytorch
-git remote add rocm_fork https://github.com/ROCmSoftwarePlatform/pytorch
-git remote add private_fork https://github.com/micmelesse/pytorch
+GITHUB_URL=https://github.com/pytorch/pytorch
+REPO_NAME=$(basename $GITHUB_URL)
+
+git remote add upstream $GITHUB_URL
+git remote add rocm_fork https://github.com/ROCmSoftwarePlatform/$REPO_NAME
+git remote add private_fork https://github.com/micmelesse/$REPO_NAME
