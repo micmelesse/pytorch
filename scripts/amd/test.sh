eaef869ca886062c289ba14cccc1117c9bb94729
Michael Melesse <micmelesse@gmail.com>
2021-06-04 19:23:24 +0000

    update test

--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -1,1 +1,5 @@
+set -e
 echo "testing"
+python -c "import torch; print(torch.__version__)"
+# pytest test/test_autograd.py
+pytest --verbose test/test_autograd.py::TestAutograd::test_anomaly_detect_nan
