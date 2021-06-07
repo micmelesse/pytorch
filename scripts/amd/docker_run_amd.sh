cc074093c96733c1282aa93489450d22f3a960f8
Michael Melesse <micmelesse@gmail.com>
2021-06-07 13:24:42 -0700

    update docker run

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -1,7 +1,10 @@
 set -o xtrace
 
-alias drun='sudo docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
-alias drun_nodevice='sudo docker run -it --rm --network=host --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
+alias drun='sudo docker run -it --rm --network=host --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
+
+DEVICES="--device=/dev/kfd --device=/dev/dri"
+
+MEMORY="--ipc=host --shm-size 16G"
 
 VOLUMES="-v $HOME/dockerx:/dockerx -v /data:/data"
 
@@ -12,13 +15,12 @@ WORK_DIR='/root/pytorch'
 # IMAGE_NAME=rocm/pytorch
 # IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
 # IMAGE_NAME=rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
-# IMAGE_NAME=rocm/pytorch-private:rocm4.0.1_ubuntu18.04_py3.6_pytorch_hipfft_issue_4
 # IMAGE_NAME=rocm/pytorch:rocm4.1.1_ubuntu18.04_py3.6_pytorch
-# IMAGE_NAME=rocm/pytorch-private:rocm4.1.1_ubuntu18.04_py3.6_pytorch_hipfft_issue_4
 # IMAGE_NAME=compute-artifactory.amd.com:5000/rocm-plus-docker/framework/compute-rocm-rel-4.2:16_ubuntu18.04_py3.6_pytorch_rocm4.2_internal_testing_b2c58d0_18
- IMAGE_NAME=rocm/pytorch-private:16_ubuntu18.04_py3.6_pytorch_rocm4.2_internal_testing_b2c58d0_18_hipfft_C2R_issue
+# IMAGE_NAME=rocm/pytorch-private:16_ubuntu18.04_py3.6_pytorch_rocm4.2_internal_testing_b2c58d0_18_hipfft_C2R_issue
+IMAGE_NAME=rocm/pytorch:rocm4.2_ubuntu18.04_py3.6_pytorch
 
-CONTAINER_ID=$(drun -d -w $WORK_DIR $VOLUMES $IMAGE_NAME)
+CONTAINER_ID=$(drun -d -w $WORK_DIR $MEMORY $VOLUMES $DEVICES $IMAGE_NAME)
 echo "CONTAINER_ID: $CONTAINER_ID"
 docker cp . $CONTAINER_ID:$WORK_DIR
 docker attach $CONTAINER_ID
